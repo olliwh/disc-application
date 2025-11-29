cb80227a1951638f7a28468c23974708d3cee418
ollwh <98967791+olliwh@users.noreply.github.com>
2025-11-29 11:54:28 +0100

    postman api tests devlop env

--- backend-disc/backend-disc/Controllers/GenericController.cs
+++ backend-disc/backend-disc/Controllers/GenericController.cs
@@ -1,6 +1,7 @@
 ﻿using backend_disc.Dtos.BaseDtos;
 using backend_disc.Repositories;
 using backend_disc.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -53,6 +54,9 @@ namespace backend_disc.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]//not admin role
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]//in valid token
+        [Authorize(Roles = "Admin,Manager")]
         public virtual async Task<IActionResult> Create([FromBody] TCreateDto createDto)
         {
             try
@@ -82,6 +86,9 @@ namespace backend_disc.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]//not admin role
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]//in valid token
+        [Authorize(Roles = "Admin,Manager")]
         public virtual async Task<IActionResult> Update(int id, [FromBody] TUpdateDto updateDto)
         {
             try
@@ -111,6 +118,9 @@ namespace backend_disc.Controllers
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]//not admin role
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]//in valid token
+        [Authorize(Roles = "Admin")]
         public virtual async Task<IActionResult> Delete(int id)
         {
             var deleted = await _service.DeleteAsync(id);
